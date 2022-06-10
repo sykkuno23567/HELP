dac5930ed87005574b06a64d43661329cfbd21a2
sykkuno23567 <86307081+sykkuno23567@users.noreply.github.com>
2022-06-10 21:43:11 +0100

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: "ODMyMzkwMjA5ODgxODk5MDM5.YHjFyw.b9YXqBPmIbKAZH2mlGvjj7lJ-EE",
+    token: "ODMyMzkwMjA5ODgxODk5MDM5.YHjFyw.b9YXqBPmIbKAZH2mlGvjj7lJ-",
     prefix: ">"
 }
